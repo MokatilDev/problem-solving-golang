81c6814143f698b381d0949cd9e574c4ba64f048
Mokatil Dev <mokatildev@gmail.com>
2026-02-05 23:26:17 +0100

    05/02/2026

---
+++ leetcode/same_tree/same_tree.go
@@ -0,0 +1,23 @@
+package same_tree
+
+type TreeNode struct {
+	Val   int
+	Right *TreeNode
+	Left  *TreeNode
+}
+
+func isSameTree(p *TreeNode, q *TreeNode) bool {
+	if p == nil && q == nil {
+		return true
+	}
+
+	if p == nil || q == nil {
+		return true
+	}
+
+	if p.Val != q.Val {
+		return false
+	}
+
+	return isSameTree(p.Left, q.Left) && isSameTree(p.Right, q.Right)
+}
