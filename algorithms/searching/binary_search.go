b1af88ccab557b8685faf2a0c4048701b6f824a7
Mokatil Dev <mokatildev@gmail.com>
2026-03-15 21:33:07 +0000

    add binary search algorithm

---
+++ algorithms/searching/binary_search.go
@@ -0,0 +1,21 @@
+package binary_search
+
+func binary_search(arr []int, target int) int {
+	low := 0
+	high := len(arr) - 1
+
+	for low <= high {
+		mid := (low + high) / 2
+		guess := arr[mid]
+
+		if target == guess {
+			return mid
+		} else if guess > target {
+			high = mid - 1
+		} else {
+			low = mid + 1
+		}
+	}
+
+	return -1
+}
